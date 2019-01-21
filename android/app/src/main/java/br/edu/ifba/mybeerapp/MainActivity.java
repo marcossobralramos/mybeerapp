084b2bf03ea65c89c9586479709f9585790bc5c0
marcossobral <amsobralramos@hotmail.com>
2019-01-21 11:55:12 -0300

    Iniciando construção dos layouts.

--- android/app/src/main/java/br/edu/ifba/mybeerapp/MainActivity.java
+++ android/app/src/main/java/br/edu/ifba/mybeerapp/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_cestas) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_lojas) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_cervejas) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_marcas) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_modelos) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_pesquisas) {
 
         }
 
